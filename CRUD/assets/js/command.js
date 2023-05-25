ea8def021c84ce0e5127ed588a740d22b60c4de8
Alan Bonfim <132375056+asbonfim@users.noreply.github.com>
2023-05-25 20:46:08 -0300

    Update command.js
    Atualizações da Issues criadas

--- CRUD/assets/js/command.js
+++ CRUD/assets/js/command.js
@@ -1,62 +1,84 @@
-console.log ('Construindo um CRUD')
+console.log ('Construindo um CRUD')//
 
-// [CRUD] JavaScript Básico
-var user = {
-  userName: ['Alan','Silva','Bonfim']
+// [CRUD] // Dados utilizados para criação dos post's
+var data = {
+  user: {
+    userName: ['Alan','Silva','Bonfim','Maria','José']
+  },
+  text: {
+    textPost: ['Bom dia!', 'Boa tarde!']
+  },
+  text2: {
+    textPost: ['Tudo bem?', 'Tenha um ótimo dia', 'Queria um sorvete', 'Preciso ir a farmacia', 'Que dia é hoje?', 'Amanhã é sexta-feira']
+  }
+};
+
+// Comando para selecionar um valor aleatório nos arrays acima:
+function randomPost(){
+  var indexUser = Math.floor(Math.random() * data.user.userName.length);
+  var indexText = Math.floor(Math.random() * data.text.textPost.length);
+  var indexText2 = Math.floor(Math.random() * data.text2.textPost.length);
+
+  return {
+    user: data.user.userName[indexUser],
+    text: `${data.text.textPost[indexText]} ${data.text2.textPost[indexText]}`,
+  };
 }
 
-var indexUser = Math.floor(Math.random() * user.userName.length); //Inseri esse comando para selecionar usuários aleatorios do arrau "User"
+console.log(randomPost)
 
+// Primeiro post que exemplifica o que estamos realizando
 const miniTwitter = {
     posts: [
       {
         id: 1,
-        ower: 'omariosouto',
         content: 'Meu primeiro tweet'
       }
     ],
   };
   
-  // CREAT
+  // CREAT - Função para criação do post
    function newPost(dados) {
-    miniTwitter.posts.push({
+    var randomValues = randomPost();
+    var newPost = {
         id: miniTwitter.posts.length + 1,
-        username: (user.userName[indexUser]), //Referencia para o comando de seleção aleatoria
-        ower: dados.ower,
-        content: dados.content
-    });
+        user: randomValues.user,
+        post: randomValues.text
+    };
+    miniTwitter.posts.push(newPost)
   }
-  newPost({ower: 'omariosouto', content: 'Segundo tweet'})
-  newPost({ower: 'Maria', content: 'Acho que estou entendento'})
-  newPost({ower:'Sabrina', content: 'Oi pessoal!'})
+
+  newPost()
+  newPost()
+  newPost()
   
   console.log(miniTwitter.posts)
 
-  // READ =
+  // READ - Função para ler os posts
   function readerPosts() {
     return miniTwitter.posts;
   }
   
 console.log(readerPosts())
 
-  //UPDATE
-  function newContentPost(id, novoConteudo) {
+  //UPDATE - Função para atualizar o content de um post
+  function newContentPost(id, newContentPost) {
     const postUpdated = readerPosts().find((post) => {
         return post.id === id;
     })
     console.log(postUpdated)
-    postUpdated.content = novoConteudo
+    postUpdated.post = newContentPost
   }
-  newContentPost(1,'Nova informação')
+  newContentPost(2,'Nova informação')
 
   console.log(readerPosts())
 
   // DELETE
   function delPost(id) {
-    const listaDePostsAtualizada = readerPosts().filter((postAtual) => {
+    const listPostUpdate = readerPosts().filter((postAtual) => {
         return postAtual.id !== id;
     })
-    miniTwitter.posts = listaDePostsAtualizada
+    miniTwitter.posts = listPostUpdate
   }
 delPost(1);
 delPost(2);
